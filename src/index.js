800ebed0b245294307e1bd42aa8842d7009fb2d6
bluelovers <codelovers@users.sourceforge.net>
2017-03-01 16:17:47 +0800

    init (Step: 3)
    Step: 3
    
    [A ] new file:
    
    	index.js
    	src/fs.js
    	src/index.js
    	src/parse.js
    	src/stringify.js
    	test/_local-dev.js
    	test/file.js
    	test/file/FE1.txt
    	test/file/FE4_cultures.txt
    	"test/file/Z00 (2).txt"
    	test/file/Z00.txt
    	test/file/ag_FE4_domestic_r12crusaders_religion.gui
    	test/file/messagetypes_custom.txt
    	test/file/remedy_portrait_traits.txt
    	test/file/settings.txt
    	test/temp/.gitkeep
    
    [ M] modified - Changes not staged for commit:
    
    	README.md

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by user on 2017/3/1.
+ */
+
+const self = Object.assign(module.exports, {
+
+		parse: require('./parse').parse,
+
+		stringify: require('./stringify').stringify,
+
+	}
+);
