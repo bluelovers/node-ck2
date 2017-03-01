0198e78ebf8abd7557c108f310f08acc3d4da609
bluelovers <codelovers@users.sourceforge.net>
2017-03-01 16:17:51 +0800

    init (Step: 7)
    Step: 7
    
    [A ] new file:
    
    	index.js
    	src/fs.js
    	src/index.js
    	src/parse.js
    	src/stringify.js
    	test/_local-dev.js
    	test/file.js
    	test/file/FE1.txt
    	test/file/FE4_cultures.txt
    	"test/file/Z00 (2).txt"
    	test/file/Z00.txt
    	test/file/ag_FE4_domestic_r12crusaders_religion.gui
    	test/file/messagetypes_custom.txt
    	test/file/remedy_portrait_traits.txt
    	test/file/settings.txt
    	test/temp/.gitkeep
    
    [ M] modified - Changes not staged for commit:
    
    	README.md

---
+++ test/file.js
@@ -0,0 +1,74 @@
+/**
+ * Created by user on 2017/3/1.
+ */
+
+'use strict';
+
+const local_dev = require('./_local-dev.js');
+const path = require('upath2');
+
+const ck2 = require('../src');
+const fs = require('fs');
+const fs2 = require('../src/fs');
+
+describe(path_relative(__filename), function ()
+	{
+		const ENCODING = 'GBK';
+
+		let _file_ = [
+			/*
+			 'D:\\Users\\Documents\\Paradox Interactive\\Crusader Kings II\\settings.txt',
+			 'D:\\Users\\Documents\\Paradox Interactive\\Crusader Kings II\\FE4\\eu4_export\\mod\\Converted_k_silesia769_09_29\\common\\countries\\Z00.txt',
+			 'D:\\Users\\Documents\\Paradox Interactive\\Crusader Kings II\\FE4\\interface\\messagetypes_custom.txt',
+			 'D:\\Users\\Documents\\Paradox Interactive\\Crusader Kings II\\FE4\\eu4_export\\mod\\Converted_k_silesia769_09_29\\history\\countries\\Z00.txt',
+			 'D:\\Users\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\FE4\\common\\cultures\\FE4_cultures.txt',
+			 'D:\\Users\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\FE4\\interface\\ag_FE4_domestic_r12crusaders_religion.gui',
+			 'D:\\Users\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\FE4\\common\\traits\\remedy_portrait_traits.txt',
+			 */
+		];
+
+		{
+			let dir = __dirname + '/file/';
+
+			let files = fs.readdirSync(dir);
+
+			files.forEach(function(file)
+			{
+				_file_.push(dir + file);
+			});
+		}
+
+		let options = {
+			//pretty: false,
+		};
+
+		describe('file', function ()
+			{
+				this.timeout(0);
+
+				_file_.forEach(function (file)
+				{
+					it(path_relative(file), () =>
+						{
+							let input = fs2.readFile(file, {
+								from: ENCODING,
+							});
+
+							let data = ck2.parse(input);
+							let output = ck2.stringify(data, options);
+							let data2 = ck2.parse(output);
+
+//							console.log(file);
+//							console.log(input);
+//							console.log(data);
+							console.log(output);
+
+							expect(data).to.deep.equal(data2);
+						}
+					);
+				});
+			}
+		);
+
+	}
+);
