03e9637e8d0f974b63cd574f1cdfbb3fa86a3b37
bluelovers <codelovers@users.sourceforge.net>
2017-03-02 19:40:23 +0800

    bugfix & add query filter (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	dom.js
    
    [ M] modified - Changes not staged for commit:
    
    	index.js
    	parse.js
    	stringify.js

--- src/index.js
+++ src/index.js
@@ -8,5 +8,47 @@ const self = Object.assign(module.exports, {
 
 		stringify: require('./stringify').stringify,
 
+		equal(...arr)
+		{
+			if (arr.length > 1)
+			{
+				let _init = true;
+				let _last;
+
+				for (let a of arr)
+				{
+					if (!a || !(typeof a == 'string' || typeof a == 'object'))
+					{
+						throw '';
+					}
+
+					let data = typeof a != 'string' ? self.stringify(a) : a;
+
+					if (_init)
+					{
+						_init = false;
+						_last = data;
+
+						continue;
+					}
+
+					if (data !== _last)
+					{
+						return false;
+					}
+
+					_last = data;
+				}
+
+				return true;
+			}
+
+			throw 'b';
+		},
+
+		selector: require('./dom').selector,
+		query: require('./dom').query,
+		filter: require('./dom').filter,
+
 	}
 );
