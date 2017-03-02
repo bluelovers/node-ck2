fadfb08c253e56ee1d750962a268bd1cfa797867
bluelovers <codelovers@users.sourceforge.net>
2017-03-02 19:49:21 +0800

    . (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	equal.js
    
    [ M] modified - Changes not staged for commit:
    
    	index.js

--- src/index.js
+++ src/index.js
@@ -8,43 +8,7 @@ const self = Object.assign(module.exports, {
 
 		stringify: require('./stringify').stringify,
 
-		equal(...arr)
-		{
-			if (arr.length > 1)
-			{
-				let _init = true;
-				let _last;
-
-				for (let a of arr)
-				{
-					if (!a || !(typeof a == 'string' || typeof a == 'object'))
-					{
-						throw '';
-					}
-
-					let data = typeof a != 'string' ? self.stringify(a) : a;
-
-					if (_init)
-					{
-						_init = false;
-						_last = data;
-
-						continue;
-					}
-
-					if (data !== _last)
-					{
-						return false;
-					}
-
-					_last = data;
-				}
-
-				return true;
-			}
-
-			throw 'b';
-		},
+		equal: require('./equal').equal,
 
 		selector: require('./dom').selector,
 		query: require('./dom').query,
